86c5497d9193e9d2e016f7c6d27243b6780c9d7c
Cebreus <cebreus@live.com>
2024-02-07 19:11:21 +0100

    refactor: move taiwind classes to generated global ones
    HTML site before 50.7 kB
    HTML site after 18.5 kB
    Global CSS 2.8 kB
    Saved 29.4 kB on one component

--- src/components/molecules/Boulder.svelte
+++ src/components/molecules/Boulder.svelte
@@ -9,13 +9,10 @@
 	let cols = 10;
 	let showAlert = false;
 
-	export const baseClass = 'w-7 h-7 text-center rounded-sm tabular-nums slashed-zero sm:w-8 sm:h-8';
-	export const baseClasses = `${baseClass} cursor-pointer bg-sky-50 border border-sky-300 hover:bg-sky-100 hover:border-sky-400 hover:text-sky-600`;
-	const skippedClass = `skipped ${baseClass}`;
-	const clickedClass = `holds ${baseClass} bg-green-400 border-green-400 text-green-900 hover:bg-green-200`;
-
-	const startClass = `${baseClass} cursor-pointer bg-sky-50 border border-sky-300 hover:bg-yellow-300 hover:border-sky-400 hover:text-sky-600 bg-yellow-400 border-green-400 text-green-900`;
-	const topClass = ` ${baseClass} cursor-pointer bg-sky-50 border border-sky-300 hover:bg-red-300 hover:border-sky-400 hover:text-sky-600 bg-red-400 border-red-400 text-red-900`;
+	const skippedClass = `skipped`;
+	const clickedClass = `holds`;
+	const startClass = `start`;
+	const topClass = `top`;
 
 	let selectingMode, selectedStartCell, selectedTopCell;
 	selector.subscribe(($selector) => {
@@ -210,24 +207,19 @@ Buttons:
 
 <Toast />
 
-<table class="wall mb-6 table-fixed border-separate text-xs sm:text-base">
+<table class="wall">
 	<thead>
 		<tr>
 			<th></th>
 			{#each Array(cols) as _, col (col)}
-				<th
-					class="h-7 w-7 rounded-sm text-center slashed-zero tabular-nums text-slate-400 sm:h-8 sm:w-8"
-					>{col}</th
-				>
+				<th>{col}</th>
 			{/each}
 		</tr>
 	</thead>
 	<tbody>
 		{#each tableRows as row, rowIndex}
 			<tr>
-				<th
-					class="h-7 w-7 rounded-sm text-center slashed-zero tabular-nums text-slate-400 sm:h-8 sm:w-8"
-				>
+				<th>
 					{String.fromCharCode(65 + rowIndex)}
 				</th>
 				{#each tableCols as col}
@@ -243,7 +235,7 @@ Buttons:
 									? clickedClass
 									: isSkippedCell(cellId)
 										? skippedClass
-										: baseClasses}
+										: ''}
 						on:click={() => {
 							toggleCell(cellId);
 						}}
@@ -267,3 +259,27 @@ Buttons:
 	></Button>
 	<Button emoji="🗑️" variant="outlineYellow" aria-label="Clear" on:click={clearBoulder}></Button>
 </div>
+
+<style lang="postcss">
+	:global(table.wall) {
+		@apply mb-6 table-fixed border-separate text-xs sm:text-base;
+	}
+	:global(table.wall th) {
+		@apply h-7 w-7 rounded-sm text-center slashed-zero tabular-nums text-slate-400 sm:h-8 sm:w-8;
+	}
+	:global(table.wall td) {
+		@apply h-7 w-7 rounded-sm text-center slashed-zero tabular-nums sm:h-8 sm:w-8;
+	}
+	:global(table.wall td:not(.skipped)) {
+		@apply cursor-pointer border border-sky-300 bg-sky-50 text-sky-600 hover:border-sky-400 hover:bg-sky-100 hover:text-sky-700;
+	}
+	:global(table.wall td.holds) {
+		@apply border-green-300 bg-green-100 text-green-600 hover:border-green-400 hover:bg-green-200 hover:text-green-700;
+	}
+	:global(table.wall td.start) {
+		@apply border-indigo-300 bg-indigo-50 text-indigo-600 hover:border-indigo-400 hover:bg-indigo-100 hover:text-indigo-700;
+	}
+	:global(table.wall td.top) {
+		@apply border-fuchsia-300 bg-fuchsia-50 text-fuchsia-600 hover:border-fuchsia-400 hover:bg-fuchsia-100 hover:text-fuchsia-700;
+	}
+</style>
