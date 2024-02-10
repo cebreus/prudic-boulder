368670e0f3365d221de82256671360f9ee4bf659
Cebreus <cebreus@live.com>
2024-02-10 14:21:44 +0100

    chore: add Sentry and Spotlight to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind(), sentry(), spotlightjs()]
 });
