bed8360fbb34e3e0cb7c9891d20b98d04c89673f
Artyshook <saibel@email.cz>
2024-02-26 12:25:04 +0100

    fix: transition + spa routing

---
+++ src/Router.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Router from 'svelte-spa-router';
+	import NewBoulder from './components/templates/NewBoulder.svelte';
+	import BouldersTable from './components/molecules/BouldersTable.svelte';
+
+	const routes = {
+		'/': BouldersTable,
+		'/new-boulder': NewBoulder,
+		'/playground': BouldersTable
+	};
+</script>
+
+<Router {routes} />
