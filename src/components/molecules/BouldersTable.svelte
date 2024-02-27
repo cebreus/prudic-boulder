5df9a18713b241bf9860c846ff5bee9833ae59ac
Artyshook <saibel@email.cz>
2024-02-27 10:42:55 +0100

    fix: dark bg

--- src/components/molecules/BouldersTable.svelte
+++ src/components/molecules/BouldersTable.svelte
@@ -118,7 +118,7 @@
 		@apply border-b bg-white dark:border-slate-700 dark:bg-slate-900;
 	}
 	table tbody tr:hover {
-		@apply bg-gray-200 outline-dotted outline-1 outline-slate-400 transition duration-500 ease-in-out;
+		@apply bg-gray-200 outline-dotted outline-1 outline-slate-400 transition duration-500 ease-in-out dark:bg-slate-700;
 	}
 	table th {
 		@apply px-3 py-2 align-bottom dark:text-slate-300;
