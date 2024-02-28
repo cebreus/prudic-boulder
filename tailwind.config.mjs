e46e0c24fdce2a1e0f7e988dff126aa523171868
Artyshook <saibel@email.cz>
2024-02-28 10:26:40 +0100

    fix: remove plugin fn

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,24 +6,5 @@ export default {
 	theme: {
 		extend: {}
 	},
-	plugins: [
-		require('@tailwindcss/typography'),
-		plugin(function ({ addUtilities }) {
-			const newUtilities = {
-				'.size-5': {
-					width: '1.25rem',
-					height: '1.25rem'
-				},
-				'.size-6': {
-					width: '1.5rem',
-					height: '1.5rem'
-				},
-				'.size-7': {
-					width: '1.75rem',
-					height: '1.75rem'
-				}
-			};
-			addUtilities(newUtilities);
-		})
-	]
+	plugins: [require('@tailwindcss/typography')]
 };
