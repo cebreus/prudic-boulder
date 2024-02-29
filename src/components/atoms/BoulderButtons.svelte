7a2d2872dfc76fc7e9283f0f927e7f6149e8119d
Artyshook <100515756+Artyshook@users.noreply.github.com>
2024-02-29 16:23:28 +0100

    refctor: move buttons from boulder + move css fn to store (#39)
    * fix: getClassFromBoulder to store
    
    * fix: remove logs
    
    * fix: trim()
    
    * fix: remove types

---
+++ src/components/atoms/BoulderButtons.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { clickedCells, selector } from '../../stores/BoulderStore.svelte';
+	import Button from './Button.svelte';
+
+	export const variant: 'default' | 'play' = 'default';
+	export let handleSaveBoulder: () => void;
+</script>
+
+{#if variant === 'default'}
+	<div class="grid w-[20.8em] grid-flow-col justify-stretch gap-4 pl-9 pr-1 pt-4 sm:w-[23.5em]">
+		<Button variant="outline" on:click={() => selector.setMode('Start')}>Start</Button>
+		<Button variant="outline" on:click={() => selector.setMode('Top')}>Top</Button>
+		<Button emoji="💾" variant="outlineGreen" aria-label="Save" on:click={handleSaveBoulder}
+		></Button>
+		<Button
+			emoji="🗑️"
+			variant="outlineYellow"
+			aria-label="Clear"
+			on:click={() => {
+				clickedCells.clear();
+				selector.clear();
+			}}
+		></Button>
+	</div>
+{/if}
