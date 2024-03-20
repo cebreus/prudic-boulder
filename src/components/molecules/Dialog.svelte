c77da15d6a9d5c643aa508ef50de98e0716d12d3
Artyshook <saibel@email.cz>
2024-03-20 09:28:52 +0100

    feat: rewrite dialog without headless ui

--- src/components/molecules/Dialog.svelte
+++ src/components/molecules/Dialog.svelte
@@ -1,84 +1,77 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
-	import {
-		Dialog,
-		DialogOverlay,
-		DialogTitle,
-		DialogDescription,
-		Transition,
-		TransitionChild
-	} from '@rgossiaux/svelte-headlessui';
+	import { createEventDispatcher, onMount } from 'svelte';
 	import { mdiClose } from '@mdi/js';
 	import Icon from '../../components/atoms/Icon.svelte';
+	import { fade } from 'svelte/transition';
 
 	export let isOpen: boolean = false;
-	const handleKeydown = (event: CustomEvent) => {
-		dispatch('keydown', event);
-	};
-
-	let onlyTitle: boolean = (!$$slots.DialogDescription && !$$slots.DialogContent) ?? true;
-
 	const dispatch = createEventDispatcher();
 
-	const closeDialog = () => {
+	function closeDialog() {
 		isOpen = false;
 		dispatch('close');
-	};
+	}
+
+	function handleKeydown(event: KeyboardEvent) {
+		if (event.key === 'Escape') {
+			closeDialog();
+		}
+		dispatch('keydown', event);
+	}
+
+	onMount(() => {
+		window.addEventListener('keydown', handleKeydown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeydown);
+		};
+	});
+
+	$: onlyTitle = !$$slots.DialogDescription && !$$slots.DialogContent;
 </script>
 
-<Transition show={isOpen}>
-	<Dialog
-		open={isOpen}
-		on:close={closeDialog}
-		class="fixed inset-0 z-10 overflow-y-auto"
-		on:keydown={handleKeydown}
-	>
-		<TransitionChild
-			as="div"
-			enter="ease-out duration-300"
-			enterFrom="opacity-0"
-			enterTo="opacity-100"
-			leave="ease-in duration-200"
-			leaveFrom="opacity-100"
-			leaveTo="opacity-0"
-		>
-			<DialogOverlay class="fixed inset-0 bg-black bg-opacity-50" />
-
-			<div class="flex min-h-full items-end justify-center p-4 py-8 sm:items-center">
-				<div
-					class="relative mx-auto max-w-md overflow-clip rounded-lg bg-white shadow-lg dark:bg-slate-800"
-				>
-					<div class="modal-body">
-						<div
-							class={`modal-header flex items-start justify-between gap-x-6 p-4 sm:p-4 ${onlyTitle ? '' : 'pb-0 sm:pb-0'}`}
-						>
-							<DialogTitle as="h6" class="text-xl font-semibold">
-								<slot name="DialogTitle" />
-							</DialogTitle>
-							<button on:click={closeDialog} class="text-gray-400 hover:text-red-600">
-								<Icon path={mdiClose} class="mt-0.5 size-6 transition-colors ease-in-out" />
-							</button>
-						</div>
-						{#if $$slots.DialogDescription || $$slots.DialogContent}
-							<div class="modal-content mb-4 mt-2 flex flex-col gap-y-4 px-4 sm:px-6">
-								{#if $$slots.DialogDescription}
-									<DialogDescription as="div">
-										<slot name="DialogDescription" />
-									</DialogDescription>
-								{/if}
-								<slot name="DialogContent" />
-							</div>
-						{/if}
-						{#if $$slots.DialogFooter}
-							<div
-								class="modal-footer m:-mx-6 flex gap-x-4 gap-y-2 bg-slate-50 px-4 sm:flex-row-reverse sm:px-6 sm:py-4 dark:bg-slate-900"
-							>
-								<slot name="DialogFooter" />
-							</div>
-						{/if}
+{#if isOpen}
+	<div class="fixed inset-0 z-10 overflow-y-auto" in:fade out:fade>
+		<div
+			class="fixed inset-0 bg-black bg-opacity-50"
+			on:click={closeDialog}
+			tabindex="0"
+			role="button"
+		></div>
+		<div class="flex min-h-full items-end justify-center p-4 py-8 sm:items-center">
+			<div
+				class="relative mx-auto max-w-md overflow-clip rounded-lg bg-white shadow-lg dark:bg-slate-800"
+			>
+				<div class="modal-body">
+					<div
+						class={`modal-header flex items-start justify-between gap-x-6 p-4 sm:p-4 ${onlyTitle ? '' : 'pb-0 sm:pb-0'}`}
+					>
+						<h6 class="text-xl font-semibold">
+							<slot name="DialogTitle" />
+						</h6>
+						<button on:click={closeDialog} class="text-gray-400 hover:text-red-600">
+							<Icon path={mdiClose} class="mt-0.5 size-6 transition-colors ease-in-out" />
+						</button>
 					</div>
+					{#if $$slots.DialogDescription || $$slots.DialogContent}
+						<div class="modal-content mb-4 mt-2 flex flex-col gap-y-4 px-4 sm:px-6">
+							{#if $$slots.DialogDescription}
+								<div>
+									<slot name="DialogDescription" />
+								</div>
+							{/if}
+							<slot name="DialogContent" />
+						</div>
+					{/if}
+					{#if $$slots.DialogFooter}
+						<div
+							class="modal-footer m:-mx-6 flex gap-x-4 gap-y-2 bg-slate-50 px-4 sm:flex-row-reverse sm:px-6 sm:py-4 dark:bg-slate-900"
+						>
+							<slot name="DialogFooter" />
+						</div>
+					{/if}
 				</div>
 			</div>
-		</TransitionChild>
-	</Dialog>
-</Transition>
+		</div>
+	</div>
+{/if}
